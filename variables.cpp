9f8ac7c85b213c7abe481346ee6c703ef7272374
Sam4d <samadpatel@protonmail.com>
2023-05-21 20:35:03 +0530

    variables update

--- variables.cpp
+++ variables.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 int glo = 6; // global variable
 void sum(){
-    int a;
+    int a = 3;
+    cout<< glo;
 }
 int main(){
     int glo = 9; // local variable
@@ -11,9 +12,12 @@ int main(){
     int b = 5;
     float pi = 3.14;
     char c = 'd';
-    cout<<"This is a tutorial. Here the value of a is "<<a<<".\n The value of b is "<<b;
-    cout<<"\n The value of pi is: "<<pi;
-    cout<<"\n The value of c is: "<<c;
-    cout<<"\n value of glo is: "<<glo;
+    bool is_true = false;
+    sum();
+    cout<<glo<< is_true;
+    // cout<<"This is a tutorial. Here the value of a is "<<a<<".\n The value of b is "<<b;
+    // cout<<"\n The value of pi is: "<<pi;
+    // cout<<"\n The value of c is: "<<c;
+    // cout<<"\n value of glo is: "<<glo;
     return 0;
 }
