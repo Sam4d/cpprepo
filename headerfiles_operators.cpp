f5d6f8bdd25092475d475326b16df97fae819b59
Sam4d <samadpatel@protonmail.com>
2023-05-24 15:42:38 +0530

    header files & operators

---
+++ headerfiles_operators.cpp
@@ -0,0 +1,22 @@
+#include<iostream> //this is a header that comes with compiler
+//#include"this.h" // this is header that can be made by the user (you need to create a file called this.h in order to use it.)
+
+using namespace std;
+
+    int main(){
+        int a = 3;
+        int b = 2;
+        cout<<"The value of a + b = "<<a+b<<endl;
+        cout<<"The value of a - b = "<<a-b<<endl;
+        cout<<"The value of a * b = "<<a*b<<endl;
+        cout<<"The value of a / b = "<<a/b<<endl;
+        cout<<"The value of a % b = "<<a%b<<endl;
+        cout<<"The value of a + b = "<<a+b<<endl;
+        cout<<"The value of a ++ = "<<a++<<endl;
+        cout<<"The value of a -- = "<<a--<<endl;
+        cout<<"The value of ++ a = "<<++a<<endl;
+        cout<<"The value of -- a = "<<--a<<endl;
+
+        return 0;
+    }
+
