7b0bf02d625ab073b8f1bfa7e949a71d8adba9ae
Sam4d <samadpatel@protonmail.com>
2023-05-25 20:39:14 +0530

    logical opearators added

--- headerfiles_operators.cpp
+++ headerfiles_operators.cpp
@@ -1,6 +1,9 @@
 #include<iostream> //this is a header that comes with compiler
 //#include"this.h" // this is header that can be made by the user (you need to create a file called this.h in order to use it.)
 
+
+// Press the keyboard shortcut Ctrl + / (Windows/Linux) to add/remove multiple comments.
+
 using namespace std;
 
     int main(){
@@ -20,15 +23,22 @@ using namespace std;
 
 // comparison operators
 
-        cout<<"The following are the comparison operators in C++: "<<endl;
-        cout<<"The value of a == b is: "<<(a==b)<<endl;
-        cout<<"The value of a < b is: "<<(a<b)<<endl;
-        cout<<"The value of a > b is: "<<(a>b)<<endl;
-        cout<<"The value of a <= b is: "<<(a<=b)<<endl;
-        cout<<"The value of a >= b is: "<<(a>=b)<<endl;
-        cout<<"The value of a != b is: "<<(a!=b)<<endl;
-        cout<<endl;
+        // cout<<"The following are the comparison operators in C++: "<<endl;
+        // cout<<"The value of a == b is: "<<(a==b)<<endl;
+        // cout<<"The value of a < b is: "<<(a<b)<<endl;
+        // cout<<"The value of a > b is: "<<(a>b)<<endl;
+        // cout<<"The value of a <= b is: "<<(a<=b)<<endl;
+        // cout<<"The value of a >= b is: "<<(a>=b)<<endl;
+        // cout<<"The value of a != b is: "<<(a!=b)<<endl; 
+        // cout<<endl;
         
+// logical operators
+
+        cout<<"The following are the logical operators in C++: "<<endl;
+        cout<<"The value of a == b AND a>b is: "<<((a==b) && (a>b))<<endl; // logical 'and' operator
+        cout<<"The value of a == b OR a>b is: "<<((a==b) || (a>b))<<endl; // logical 'or' operator
+
+
 
         return 0;
     }
