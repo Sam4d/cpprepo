9ee8c44c0a2b40744269f64136462a4bce4680ac
Sam4d <samadpatel@protonmail.com>
2023-05-26 10:57:19 +0530

    not final

--- const_mani_operators.cpp
+++ const_mani_operators.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<iomanip>
+
 using namespace std;
 
 int main(){
@@ -15,9 +17,21 @@ int main(){
 
 // constant in C++
 
-const int a = 69;
-cout<<"The value of a is: "<<a<<endl;
+// const int a = 69;
+// cout<<"The value of a is: "<<a<<endl;
 
-a = 44; // we get this error because we already declared the value of a as 69 but constant.
+// a = 44; // we get this error because we already declared the value of a as 69 but constant.
+
+// Another manipulator (setw) - this is used to right justify your code in C++ (RHS)
+
+int x = 33,y = 7, z = 4324;
+
+cout<<"The value of x is ="<<x<<endl;
+cout<<"The value of y is ="<<y<<endl;
+cout<<"The value of z is ="<<z<<endl;
+
+cout<<"The value of x with setw is ="<<setw(4)<<x<<endl;
+cout<<"The value of y with setw is ="<<setw(4)<<y<<endl;
+cout<<"The value of z with setw is ="<<setw(4)<<z<<endl;
 
 }
