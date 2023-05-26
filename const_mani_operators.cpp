8fc76cb6f7dc8b3e003ccca3399ed3b8595a013f
Sam4d <samadpatel@protonmail.com>
2023-05-26 10:42:45 +0530

    initial

---
+++ const_mani_operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // int a = 34;
+    // char c = 'd';
+    
+    // a = 45;
+    // c = 'e';
+
+    // cout<<"The value of a was: "<<a<<endl;
+    // cout<<"The value of c was: "<<c<<endl;
+    // return 0;
+
+// constant in C++
+
+const int a = 69;
+cout<<"The value of a is: "<<a<<endl;
+
+a = 44; // we get this error because we already declared the value of a as 69 but constant.
+
+}
