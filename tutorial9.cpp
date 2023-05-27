d276075e7816d74c0ff4e5ce89cbc49f16c76464
Sam4d <samadpatel@protonmail.com>
2023-05-27 08:49:12 +0530

    If Else and Switch-Case Statement

--- tutorial9.cpp
+++ tutorial9.cpp
@@ -15,19 +15,55 @@
 
 using namespace std;
 
+// int main(){
+//     int age;
+//  //   cout<<"Hello World";
+//     cout<<"What is your age?: "<<endl;
+//     cin>>age;
+//     if((age<18) && (age>0)){
+//         cout<<"Sorry you can't come to my party"<<endl;
+//     }
+//     else if(age < 1){
+//         cout<<"You're not even born yet bruh"<<endl;
+//     }
+//     else if(age ==18){
+//         cout<<"Yay you can come to my party!"<<endl;
+//     }
+//     else{
+//         cout<<"You can come to the party bro, gotchu"<<endl;
+//     }
+
+//     return 0;
+// }
+
+// Control Structure: Switch Case
+
 int main(){
-    int age;
- //   cout<<"Hello World";
-    cout<<"What is your age?: "<<endl;
+int age;
+    cout<<"What is your age? "<<endl;
     cin>>age;
-    if(age<18){
-    cout<<"Sorry you can't come to my party"<<endl;
+
+switch (age)
+    {
+case 18:
+    cout<<"You're 18 brother!"<<endl;
+    break;
+
+case 23:
+    cout<<"You're 23 brother!"<<endl;
+    break;
+
+case 12:
+    cout<<"You're 12 brother!"<<endl;
+    break;
+
+case 19:
+    cout<<"You're 19 brother!"<<endl;
+    break;
+
+default:
+    cout<<"No special case, dumb age"<<endl;
     }
-    else if(age ==18){
-    cout<<"Yay you can come to my party!"<<endl;
-    }
-    else{
-    cout<<"You can come to the party bro, gotchu"<<endl;
-    }
-    return 0;
+    
+    cout<<"Done with the process"<<endl;
 }
