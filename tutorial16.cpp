3fee922fb647c7391456a2cc709c70b26575ad7c
Sam4d <samadpatel@protonmail.com>
2023-06-05 16:44:15 +0530

    still incomplete

--- tutorial16.cpp
+++ tutorial16.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
+void swap(int a, int b){  //  temp  a   b
+    int temp = a;          //   4   4   5
+    a = b;                 //   4   5   5
+    b = temp;              //   4   5   4
+}
+
 int main(){
-    cout<<"Hello world";
+    
+    int a = 4, b = 5;
+    cout<<"The value of a is: "<<a<<" And the value of b is: "<<b<<endl;
+    swap(a,b);
+    cout<<"The value of a is: "<<a<<" And the value of b is: "<<b<<endl;
+
     return 0;
 }
