12eb598ad35eb04dc4de8be1f1d12e210270c1ef
Sam4d <samadpatel@protonmail.com>
2023-06-14 21:19:24 +0530

    Object Oriented Programming in C++

---
+++ tutorial20.cpp
@@ -0,0 +1,37 @@
+/* Object Oriented Programming in C++
+
+    C++ was made to add OOP to C
+    As size of program increases, the readability, maintainability & bug free nature of program decreases
+    this was a major problem in C which relied upon functions & procedures
+
+ ** Procedural programming language **
+
+    Consists of writing set of functions for computer to follow 
+    Main focus is on functions & not flow of data
+    Functions can either use local or global data
+    Data moves openly from function to function 
+
+** Object Orinted programming language **
+
+    Works on concept of classes & objects
+    A class is a template to create objects
+    Treats data as a critical element
+    Decomposes the problem in objects and builds data & functions around the objects
+
+
+*** Concepts in OOP ***
+
+    Classes - Basic Templates
+    Objects - Basic run time entities
+    Data abstraction & encapsulation - Wrapping data & functions in a single unit
+    Inheretance - Properties of one class can be inherented into others
+    Polymorphism - ability to take more than one forms
+    Dynamic binding - code which will execute is not known until the program runs
+    Message passing - Object.message(Information) call format
+
+*** Benefits of OOP ***
+
+    Better code reusability using objects & inheretance
+    Principle of data handling helps build secure systems 
+    Multiple objects can co-exist without any interference
+    Software complexity can be easily managed */
